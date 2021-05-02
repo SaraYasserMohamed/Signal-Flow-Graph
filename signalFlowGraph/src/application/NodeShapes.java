deef0497e31eaeb7b04f4c5ad49f9dc8859d14a6
ahmed <ahmed@192.168.1.6>
2021-05-02 16:11:25 +0200

    set calculations

--- signalFlowGraph/src/application/NodeShapes.java
+++ signalFlowGraph/src/application/NodeShapes.java
@@ -7,56 +7,59 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class NodeShapes {
-	
-	private int ScreenWidth = 1200;
-	private int NodeNum = 2;
+
+	private int ScreenWidth = 1200, NodeNum = 2;
+	private double radius, distance;
 	private Group root;
 	private ArrayList<Circle> circles;
-	
+
 	public NodeShapes(Group root) {
 		this.root = root;
 		circles = new ArrayList<Circle>();
 		initialize();
 	}
-	
+
 	private void SetNodeShapes() {
-		double radius = 200/NodeNum;
-		double distance  = ScreenWidth/NodeNum;
 		Circle c;
-		for (int i = 0 ; i < NodeNum;i++) {
-			c = new Circle(i*distance+distance/2,350,radius);
-			if (i == 0) 
+		for (int i = 0; i < NodeNum; i++) {
+			c = new Circle(i * distance + distance / 2, 350, radius);
+			if (i == 0)
 				c.setFill(Color.GREEN);
-			else if (i == NodeNum-1)
+			else if (i == NodeNum - 1)
 				c.setFill(Color.RED);
 			else
 				c.setFill(Color.GRAY);
 			circles.add(c);
 		}
 	}
+
 	private void initialize() {
+		setCalculations();
 		SetNodeShapes();
 		this.root.getChildren().addAll(circles);
 	}
-	
+
 	public void AddNode() {
 		NodeNum++;
-		double radius = 200/NodeNum;
-		double distance  = ScreenWidth/NodeNum;
-		Circle c = null;int i;
-		for (i = 0 ; i < circles.size() ; i++) {
+		setCalculations();
+		Circle c = null;
+		int i;
+		for (i = 0; i < circles.size(); i++) {
 			c = circles.get(i);
-			c.setCenterX(i*distance+distance/2);
+			c.setCenterX(i * distance + distance / 2);
 			c.setRadius(radius);
 		}
-		if (i!=0)
+		if (i != 0)
 			c.setFill(Color.GRAY);
-		c = new Circle(i*distance+distance/2,350,radius);
+		c = new Circle(i * distance + distance / 2, 350, radius);
 		c.setFill(Color.RED);
 		circles.add(c);
 		root.getChildren().add(c);
 	}
-	
+	private void setCalculations() {
+		radius = 200 / NodeNum;
+		distance = ScreenWidth / NodeNum;
+	}
 
 	public ArrayList<Circle> getNodes() {
 		return circles;
