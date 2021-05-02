306fd2ac41778a2fe1738fc7261ca7d02d048cbe
ahmed <ahmed@192.168.1.6>
2021-05-02 13:16:10 +0200

    graph implementation

---
+++ signalFlowGraph/src/application/Graph.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.ArrayList;
+
+public class Graph {
+	private ArrayList<BasicNode> graph;
+	public Graph() {
+		graph = new ArrayList<BasicNode>();
+	}
+	public void addNewBasicNode(int id) {
+		graph.add(new BasicNode(id));
+	}
+	public void addNode(int BasicNodeID , int NodeID ,String gain) {
+		graph.get(BasicNodeID).AddnewNode(new Node(NodeID,gain));
+	}
+	public String getGain (int BasicNodeID,int NodeID) {
+		return graph.get(BasicNodeID).getNode(NodeID).getGain();
+	}
+	public ArrayList<BasicNode> getGraph() {
+		return graph;
+	}
+}
---
+++ signalFlowGraph/src/application/Node.java
@@ -0,0 +1,17 @@
+package application;
+
+public class Node {
+	private int id;
+	private String gain;
+	public Node(int id ,String gain) {
+		this.id = id;
+		this.gain = gain;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getGain() {
+		return gain;
+	}
+
+}
